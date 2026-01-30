ebe7ef84a64f5a2d2ec3abb8651c755123680ed6
Chelsea <500196039@fvtc.edu>
2026-01-30 12:16:58 -0500

    Added welcome div

--- site.js
+++ site.js
@@ -1,1 +1,23 @@
-console.log("Well hello there!")
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+// Selecting the welcome div
+const welcome = document.querySelector("#welcome")
+
+// Creating the h3 element
+const greeting = document.createElement("h3")
+
+// Appending it to the welcome div
+welcome.append(greeting)
+
+// Setting the greeting text based on the time of the day
+if (isMorning) {
+    greeting.textContent = "Good Morning! Welcome to my website."
+} else if (isAfternoon) {
+    greeting.textContent = "Good Afternoon! Welcome to my website."
+} else if (isEvening) {
+    greeting.textContent = "Good Evening! Welcome to my website."
+}
