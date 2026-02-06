d7b2eb8b3b61f62b3b60ce79474f6d2c38cafc88
Chelsea <500196039@fvtc.edu>
2026-02-06 13:38:30 -0500

    Added secret message

--- site.js
+++ site.js
@@ -4,6 +4,8 @@ const isMorning = hours >= 4 && hours < 12 // is it morning?
 const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
 const isEvening = hours >= 17 || hours < 4 // is it evening?
 
+localStorage.setItem("It's a secret to everybody.", "Eastmost peninsula is the secret.")
+
 // Selecting the welcome div
 const welcome = document.querySelector("#welcome")
 
